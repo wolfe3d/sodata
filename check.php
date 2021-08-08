ce855763df19d2d5318614dda31b84c2e5418a96
wolfe3d <44325097+wolfe3d@users.noreply.github.com>
2021-08-08 08:33:59 -0400

    update

---
+++ check.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration
+$mysqlConn= mysqli_connect('localhost', 'ggUs3963!er', '3DP2PuMsHwzXRpXR', 'scienceolympiad');
+
+/* check connection */
+if ($mysqlConn->connect_errno) {
+   die("Failed to connect with MySQL: " . $mysqlConn->connect_error);
+}
+?>
