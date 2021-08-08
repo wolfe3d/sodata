cea91b920a71f3b51d47059c99cbd411b8274b14
wolfe3d <44325097+wolfe3d@users.noreply.github.com>
2021-08-08 08:37:51 -0400

    1

--- check.php
+++ check.php
@@ -7,6 +7,12 @@ if ($mysqlConn->connect_errno) {
    die("Failed to connect with MySQL: " . $mysqlConn->connect_error);
 }
 else {
+
 		echo "okay";
+		$query = "SELECT * FROM `coach` WHERE `coach`.`coachID` = 1";
+		$result = $mysqlConn->query($query) or error_log("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
+		$row = $result->fetch_assoc();
+		echo $row['first'];
+		echo "okay2";
 }
 ?>
