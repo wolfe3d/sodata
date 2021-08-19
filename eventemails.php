23cbf59f269c594506596fa8cf97ee413255f22d
Chinmay Joshi <chinmaypjoshi21@gmail.com>
2021-08-18 20:11:18 -0400

    fixed event emails query, int rather than string comparison was being made to compare dates

--- eventemails.php
+++ eventemails.php
@@ -9,9 +9,11 @@ $event = intval($_POST['myID']);
 //semester teams tournament hardcoded, change later
 $emails = "";
 $fallRosterDate = strval(getCurrentSOYear()-1)."-07-31";
-
-$query = "SELECT DISTINCT `first`, `last`, `email`, `emailSchool` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and dateTournament > $fallRosterDate";
+$emails.=$fallRosterDate;
+$query = "SELECT DISTINCT `first`, `last`, `email`, `emailSchool` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and dateTournament > '$fallRosterDate'";
 $result = $mysqlConn->query($query) or print("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
+$emails.=$query;
+
 while ($row = $result->fetch_assoc()):
     if($row['email']){
         $emails.=$row['first'] . " " . $row['last']." <";
