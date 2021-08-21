3625eb134149bdee0f31f54849e2af8f8d035d63
Chinmay Joshi <chinmaypjoshi21@gmail.com>
2021-08-21 12:01:52 -0400

    cleaned up event emails interface with line breaks and removing redundant emails (email of the user)

--- eventemails.php
+++ eventemails.php
@@ -6,24 +6,26 @@ require_once  ("functions.php");
 userCheckPrivilege(2);
 
 $event = intval($_POST['myID']);
+$studentID = getStudentID($mysqlConn, $_SESSION['userData']['id']);
 //semester teams tournament hardcoded, change later
 $emails = "";
 $fallRosterDate = strval(getCurrentSOYear()-1)."-07-31";
-$query = "SELECT DISTINCT `first`, `last`, `email`, `emailSchool` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and dateTournament > '$fallRosterDate'";
+$query = "SELECT DISTINCT `first`, `last`, `email`, `emailSchool` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and dateTournament > '$fallRosterDate' and `student`.`studentID` != $studentID";
 $result = $mysqlConn->query($query) or print("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
 
 while ($row = $result->fetch_assoc()):
     if($row['email']){
-        $emails.=$row['first'] . " " . $row['last']." <";
-        $emails.=$row['email'] . ">; ";
+        $emails.=$row['first'] . " " . $row['last']." &lt;";
+        $emails.=$row['email'] . "&gt;; ";
     }
 
     if($row['emailSchool']){
-        $emails.=$row['first'] . " " . $row['last'] . " <";
-        $emails.=$row['emailSchool'] . ">; ";
+        $emails.="&lt;".$row['emailSchool'] . "&gt;; ";
     }
+    $emails.="<br>";
 
 endwhile;
 
-echo htmlspecialchars($emails);
+// echo htmlspecialchars($emails);
+echo $emails;
 ?>
