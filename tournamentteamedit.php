2449e159042dd0082e2a6e0efbf8da492f010394
wolfe3d <44325097+wolfe3d@users.noreply.github.com>
2021-12-20 22:23:38 -0500

    Deactivated students assigned.  This helps report unassigned students.

--- tournamentteamedit.php
+++ tournamentteamedit.php
@@ -9,6 +9,27 @@ require_once  ("functions.php");
 	*Warn if under 15 students
 	*Error if over 15 students OR over 7 seniors
 */
+
+function assignedToTeam($db, $teamID, $studentID)
+{
+	//$query = "SELECT * FROM `teammate` INNER JOIN `team` ON `teammate`.`teamID`=`team`.`teamID` WHERE `teammate`.`teamID` = $teamID";
+	$query = "SELECT * FROM `teammate` WHERE `teamID` =  $teamID AND `studentID` = $studentID" ;
+	$result = $db->query($query) or error_log("\n<br />Warning: query failed:$query. " . $db->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
+	return mysqli_num_rows($result);
+}
+function assignedToOtherTeam($db, $tournamentID, $studentID)
+{
+	//$query = "SELECT * FROM `teammate` INNER JOIN `team` ON `teammate`.`teamID`=`team`.`teamID` WHERE `teammate`.`teamID` = $teamID";
+	$query = "SELECT * FROM `teammate` INNER JOIN `team` ON `teammate`.`teamID`=`team`.`teamID` WHERE `team`.`tournamentID` =  $tournamentID AND `studentID` = $studentID" ;
+	$result = $db->query($query) or error_log("\n<br />Warning: query failed:$query. " . $db->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
+	$row = $result->fetch_assoc();
+	if(empty($row))
+	{
+		return "";
+	}
+	return "(Assigned to team " . $row['teamName'] . ")";
+}
+
 $teamID= intval($_POST['myID']);
 if(empty($teamID))
 {
@@ -23,19 +44,30 @@ if(empty($row))
 	exit("<div style='color:red'>No team found with ID: $teamID.</div>");
 }
 $teamName = $row['teamName'];
-
-$studentList="<div><input type='checkbox' id='toggleInactive' name='toggleInactive' onchange='$(\".inactive\").toggle()' /><label for='toggleInactive'>Show Inactive Students</label></div>";
+$tournamentID = $row['tournamentID'];
+$studentList="<div class='scioly'><input type='checkbox' id='toggleInactive' name='toggleInactive' onchange='$(\".inactive\").toggle()' /><label for='toggleInactive'>Show Inactive Students</label></div>";
 $query = "SELECT * FROM `student` ORDER BY `last` ASC, `first` ASC";
 $resultStudent = $mysqlConn->query($query) or error_log("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
 if($resultStudent){
 	while ($rowStudent = $resultStudent->fetch_assoc()):
 		$checkbox = "teammate-".$teamID."-".$rowStudent['studentID'];
-		$query = "SELECT * FROM `teammate` WHERE `teamID` =  $teamID AND `studentID` = ".$rowStudent['studentID'] ;
-		$resultTeammate= $mysqlConn->query($query) or error_log("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
+
+		//$query = "SELECT * FROM `teammate` WHERE `teamID` =  $teamID AND `studentID` = ".$rowStudent['studentID'] ;
+		//$resultTeammate= $mysqlConn->query($query) or error_log("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
 		//if there is a result then make box checked, if not do not check box.
-		$checked = mysqli_num_rows($resultTeammate)?" checked ":"";
+		$checked = assignedToTeam($mysqlConn, $teamID, $rowStudent['studentID'])?" checked ":"";
+		$assigned = "";
+		$disabled = "";
+		if (!$checked)
+		{
+			$assigned = assignedToOtherTeam($mysqlConn, $tournamentID, $rowStudent['studentID']);
+			if($assigned)
+			{
+				$disabled = " disabled='disabled' ";
+			}
+		}
 		$hidden = $rowStudent['active']?"":"class='inactive' style='display: none;'";
-		$studentList .= "<div $hidden><input type='checkbox' data-studentgrade='".getStudentGrade($rowStudent['yearGraduating'])."' onchange='javascript:tournamentTeammate($(this))' id='$checkbox' name='$checkbox' value='' $checked><label for='$checkbox'><a target='_blank' href='#student-details-".$rowStudent['studentID']."'>".$rowStudent['last'].", " . $rowStudent['first'] ." - " . getStudentGrade($rowStudent['yearGraduating']) ."</a></label></div>";
+		$studentList .= "<div $hidden class='scioly'><input type='checkbox' data-studentgrade='".getStudentGrade($rowStudent['yearGraduating'])."' onchange='javascript:tournamentTeammate($(this))' id='$checkbox' name='$checkbox' value='' $checked $disabled><label for='$checkbox'><a target='_blank' href='#student-details-".$rowStudent['studentID']."'>".$rowStudent['last'].", " . $rowStudent['first'] ." - " . getStudentGrade($rowStudent['yearGraduating']) ."</a> $assigned</label></div>";
 	endwhile;
 }
 ?>
