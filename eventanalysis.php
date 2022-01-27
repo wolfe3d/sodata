f966f78f91e3d944a2e723b398ce39d4f9a9b087
wolfe3d <44325097+wolfe3d@users.noreply.github.com>
2022-01-26 21:27:13 -0500

    Barely making analysis much work to do

--- eventanalysis.php
+++ eventanalysis.php
@@ -5,47 +5,41 @@ require_once  ("checksession.php"); //Check to make sure user is logged in and h
 require_once  ("functions.php");
 userCheckPrivilege(2);
 
-
-
 $event = intval($_POST['myID']);
 $studentID = getStudentID($mysqlConn, $_SESSION['userData']['userID']);
 //semester teams tournament hardcoded, change later
-$emails = "<h2>TODO: ANALYSIS".getEventName($mysqlConn,$event)."</h2>";
+$output = "<h2>".getEventName($mysqlConn,$event)." Analysis</h2>";
 $fallRosterDate = strval(getCurrentSOYear()-1)."-08-01";
-$emails.="<h3>Fall Roster</h3>";
-$query = "SELECT DISTINCT `first`, `last`, `email`, `emailSchool` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and dateTournament = '$fallRosterDate' and `student`.`studentID` != $studentID and `student`.`active` = 1";
+$query = "SELECT `first`, `last`, `email`, `emailSchool`,`place`,`tournamentName` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and `student`.`active` = 1 and `place` IS NOT NULL Order By `last`, `first`";
 $result = $mysqlConn->query($query) or print("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
 
 while ($row = $result->fetch_assoc()):
     if($row['email']){
-        $emails.=$row['first'] . " " . $row['last']." &lt;";
-        $emails.=$row['email'] . "&gt;; ";
+        $output.=$row['first'] . " " . $row['last']." ";
+        $output.=$row['tournamentName'] . " " . $row['place'];
     }
 
-    if($row['emailSchool']){
-        $emails.="&lt;".$row['emailSchool'] . "&gt;; ";
-    }
-    $emails.="<br>";
+    $output.="<br>";
 
 endwhile;
 
 $query = "SELECT DISTINCT `first`, `last`, `email`, `emailSchool` FROM `tournamentevent` INNER JOIN `teammateplace` ON `tournamentevent`.`tournamenteventID` = `teammateplace`.`tournamenteventID` INNER JOIN `tournament` on `tournamentevent`.`tournamentID` = `tournament`.`tournamentID` INNER JOIN `student` ON `teammateplace`.`studentID` = `student`.`studentID` WHERE eventID = $event and `student`.`studentID` != $studentID and `student`.`active` = 1 group by `first`, `last`,`email`,`emailSchool` having sum(case when `dateTournament` > '$fallRosterDate' then 1 else 0 end) > 0 AND sum(case when `dateTournament` = '$fallRosterDate' then 1 else 0 end) = 0";
 $result = $mysqlConn->query($query) or print("\n<br />Warning: query failed:$query. " . $mysqlConn->error. ". At file:". __FILE__ ." by " . $_SERVER['REMOTE_ADDR'] .".");
 if($result->num_rows>0){
-    $emails.="<br><br><h3>Other students assigned to tournaments</h3>";
+    $output.="<br><br><h3>Other students assigned to tournaments</h3>";
     while ($row = $result->fetch_assoc()):
         if($row['email']){
-            $emails.=$row['first'] . " " . $row['last']." &lt;";
-            $emails.=$row['email'] . "&gt;; ";
+            $output.=$row['first'] . " " . $row['last']." &lt;";
+            $output.=$row['email'] . "&gt;; ";
         }
 
         if($row['emailSchool']){
-            $emails.="&lt;".$row['emailSchool'] . "&gt;; ";
+            $output.="&lt;".$row['emailSchool'] . "&gt;; ";
         }
-        $emails.="<br>";
+        $output.="<br>";
 
     endwhile;
 }
-$emails.= "<br><br><input class='button fa' type='button' onclick=\"window.location='#events'\" value='&#xf0a8; Return' />";
-echo $emails;
+$output.= "<br><br><input class='button fa' type='button' onclick=\"window.location='#events'\" value='&#xf0a8; Return' />";
+echo $output;
 ?>
