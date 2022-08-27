3b40ba032b7c9ad6b899b638be95c395cb67c5a7
wolfe3d <44325097+wolfe3d@users.noreply.github.com>
2022-08-26 21:56:38 -0400

    team email bug

--- tournamentemails.php
+++ tournamentemails.php
@@ -4,7 +4,7 @@ require_once  ("php/functions.php");
 userCheckPrivilege(3);
 $team = intval($_POST['myID']);
 
-echo getTeamEmails($mysqlConn, NULL, $tournamentID,0);?>
+echo getTeamEmails($mysqlConn, $team, NULL,0);?>
 <p>
 <button class='btn btn-outline-secondary' onclick='window.history.back()' type='button'><span class='fa fa-arrow-circle-left'></span> Return</button>
 </p>
