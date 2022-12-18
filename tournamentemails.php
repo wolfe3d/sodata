f23d5fc3298409cf0f89f9e035db5295a5411f52
wolfe3d <44325097+wolfe3d@users.noreply.github.com>
2022-12-18 15:31:47 -0500

    Update tournamentemails.php
    TeamID changed to tournamentID

--- tournamentemails.php
+++ tournamentemails.php
@@ -2,9 +2,9 @@
 header("Content-Type: text/plain");
 require_once  ("php/functions.php");
 userCheckPrivilege(3);
-$team = intval($_POST['myID']);
+$tournamentID = intval($_POST['myID']);
 
-echo getTeamEmails($mysqlConn, $team, NULL,0);?>
+echo getTeamEmails($mysqlConn, NULL, $tournamentID,0);?>
 <p>
 <button class='btn btn-outline-secondary' onclick='window.history.back()' type='button'><span class='fa fa-arrow-circle-left'></span> Return</button>
 </p>
